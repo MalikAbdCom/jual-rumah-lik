e1dec089ca6657bfa4a70176faf1ac51f1c42679
malikabdcom <emailmalikabdcom@gmail.com>
2023-03-25 10:44:39 +0700

    adding 'update name' on profile page and save changes to firestore

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { doc, updateDoc } from "firebase/firestore";
+
+import { toast } from "react-toastify";
+
+import { db } from "../firebase.config";
 
 const Profile = () => {
   const auth = getAuth();
@@ -10,6 +15,8 @@ const Profile = () => {
     name: "",
     email: "",
   });
+  const [editChange, setEditChange] = useState(false);
+
   const { name, email } = inputValue;
 
   // get user info
@@ -32,27 +39,82 @@ const Profile = () => {
     navigate("/SignIn");
   };
 
+  function editChangeFunc(event) {
+    setEditChange(!editChange);
+  }
+
+  function updateProfileName() {
+    (async () => {
+      updateProfile(auth.currentUser, name);
+
+      // update progile data on firestore
+
+      const user = auth.currentUser;
+
+      const docRef = doc(db, "users", user.uid);
+
+      await updateProfile(user, {
+        displayName: name,
+      });
+
+      await updateDoc(
+        docRef,
+        {
+          name: name,
+        },
+        { merge: true }
+      )
+        .then(() => {
+          toast.success("Update success");
+        })
+        .catch((error) => {
+          console.log(error);
+          toast.error("Update failed");
+        });
+    })();
+  }
+
   return (
     <div className="max-w-6xl flex flex-col justify-center items-center mx-auto p-2">
       <h1 className="text-center text-3xl font-bold p-6 mb-6">Profile</h1>
-      <div className="max-w-[768px]">
+      <div className="w-full max-w-[500px]">
         <input
           type={"text"}
-          className={"w-full p-2 mb-4 rounded text-slate-700"}
+          className={`w-full p-2 mb-4 rounded text-slate-700 ${
+            editChange && "bg-red-200 font-bold"
+          }`}
+          onChange={(e) =>
+            setInputValue((prevState) => {
+              return {
+                ...prevState,
+                [e.target.name]: e.target.value,
+              };
+            })
+          }
+          name={"name"}
           value={name}
-          disabled={true}
+          disabled={!editChange}
         />
         <input
           type={"email"}
           className={"w-full p-2 mb-4 rounded text-slate-700"}
           value={email}
           disabled={true}
+          name={"email"}
         />
 
         <div className="whitespace-nowrap flex justify-between w-full mb-4 text-sm md:text-base">
           <p>
-            Do want to change your name?{" "}
-            <span className="text-red-400 ml-1 cursor-pointer">Edit</span>
+            Do want to change your name?
+            <span
+              className="text-red-400 ml-1 cursor-pointer"
+              onClick={() => {
+                editChange && updateProfileName();
+                editChangeFunc();
+              }}
+            >
+              {editChange ? "Apply change" : "Edit"}
+            </span>
           </p>
           <p
             onClick={onSignOut}
