857a9d6563968dfa8b527451557491ea35cd696c
malikabdcom <emailmalikabdcom@gmail.com>
2023-05-11 10:35:23 +0700

    menampilkan hasil registrasi item ke profile sebagi list card

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,8 +1,66 @@
-export const ListingItem = ({listing, id}) => {
-    return (
-        <div>
-            <p>{id}</p>
-            <p>{listing.name}</p>
+import { Link } from "react-router-dom";
+import TimeAgo from "react-timeago";
+import indoStrings from "react-timeago/lib/language-strings/id";
+import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
+import { MdLocationOn } from "react-icons/md";
+
+const ListingItem = ({ listing, id }) => {
+  const formatter = buildFormatter(indoStrings);
+  const fromDate = listing.timestamp.toDate().toString();
+
+  console.log(listing);
+
+  const harga = [listing.regularPrice, listing.discountedPrice];
+  const hasilConvertHarga = harga.map((item) => {
+    return new Intl.NumberFormat("en-US", {
+      style: "decimal",
+      minimumFractionDigits: 2,
+    }).format(item);
+  });
+  const hargaDiTampilkan = function () {
+    const hargaAkhir = listing.discountedPrice
+      ? hasilConvertHarga[1]
+      : hasilConvertHarga[0];
+
+    const penambahanStatus = listing.type === "rent" ? "/month" : "";
+
+    return hargaAkhir + penambahanStatus;
+  };
+
+  return (
+    <li className="relative bg-white text-teal-900 mb-8 flex flex-col justify-between shadow-blue-400 shadow-md rounded overflow-hidden transition duration-300 max-w-[300px]">
+      <Link className="block" to={`/category/${listing.type}/${id}`}>
+        <div className="h-[170px] overflow-hidden">
+          <img
+            className="w-full h-[170px] transition hover:scale-110 duration-150 object-cover"
+            src={listing.imgUrls[0]}
+            alt={listing.name}
+          />
         </div>
-    )
+        <TimeAgo
+          date={fromDate}
+          formatter={formatter}
+          className="absolute top-2 left-2 px-2 py-1 bg-blue-500 text-white font-semibold capitalize text-xs rounded shadow shadow-blue-300"
+        />
+        <div className="p-2">
+          <div className="flex gap-1 items-center text-sm">
+            <p>
+              <MdLocationOn className="text-blue-500 text-md" />
+            </p>
+            <p className="truncate">{listing.address}</p>
+          </div>
+          <h3 className="text-xl font-semibold text-teal-800">
+            {listing.name}
+          </h3>
+          <p className="text-red-400 font-bold">Rp. {hargaDiTampilkan()}</p>
+          <div className="flex gap-2 text-sm mt-1">
+            <p>{`${listing.bedrooms} Kamar`}</p>
+            <p>{`${listing.bathrooms} WC`}</p>
+          </div>
+        </div>
+      </Link>
+    </li>
+  );
 };
+
+export default ListingItem;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,187 +1,191 @@
-import {useEffect, useState} from "react";
-import {getAuth, updateProfile} from "firebase/auth";
-import {Link, useNavigate} from "react-router-dom";
-import {collection, doc, getDocs, orderBy, query, updateDoc, where} from "firebase/firestore";
-import {FcHome} from "react-icons/fc";
+import { useEffect, useState } from "react";
+import { getAuth, updateProfile } from "firebase/auth";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  collection,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { FcHome } from "react-icons/fc";
 
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
-import {db} from "../firebase.config";
-import {ListingItem} from "../components/ListingItem.jsx";
+import { db } from "../firebase.config";
+import ListingItem from "../components/ListingItem.jsx";
 
 const Profile = () => {
-    const auth = getAuth();
-    const navigate = useNavigate();
-    const [listings, setListings] = useState([])
-    const [loading, setLoading] = useState(false)
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [listings, setListings] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const [inputValue, setInputValue] = useState({
-        name: "",
-        email: "",
-    });
-    const [editChange, setEditChange] = useState(false);
+  const [inputValue, setInputValue] = useState({
+    name: "",
+    email: "",
+  });
+  const [editChange, setEditChange] = useState(false);
 
-    const {name, email} = inputValue;
+  const { name, email } = inputValue;
 
-    // get user info
-    useEffect(() => {
-        const getUserInfo = async () => {
-            const user = auth.currentUser;
-            if (user) {
-                setInputValue({
-                    name: user.displayName,
-                    email: user.email,
-                });
-            }
-        };
-
-        getUserInfo();
-    }, []);
-
-    const onSignOut = () => {
-        auth.signOut();
-        navigate("/SignIn");
+  // get user info
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const user = auth.currentUser;
+      if (user) {
+        setInputValue({
+          name: user.displayName,
+          email: user.email,
+        });
+      }
     };
 
-    function editChangeFunc(event) {
-        setEditChange(!editChange);
+    getUserInfo();
+  }, []);
+
+  const onSignOut = () => {
+    auth.signOut();
+    navigate("/SignIn");
+  };
+
+  function editChangeFunc(event) {
+    setEditChange(!editChange);
+  }
+
+  function updateProfileName() {
+    (async () => {
+      updateProfile(auth.currentUser, name);
+
+      // update progile data on firestore
+
+      const user = auth.currentUser;
+
+      const docRef = doc(db, "users", user.uid);
+
+      await updateProfile(user, {
+        displayName: name,
+      });
+
+      await updateDoc(
+        docRef,
+        {
+          name: name,
+        },
+        { merge: true }
+      )
+        .then(() => {
+          toast.success("Update success");
+        })
+        .catch((error) => {
+          console.log(error);
+          toast.error("Update failed");
+        });
+    })();
+  }
+
+  useEffect(() => {
+    async function fetchUserListings() {
+      const listingRef = collection(db, "listings");
+      const q = query(
+        listingRef,
+        where("userRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      );
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
     }
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
 
-    function updateProfileName() {
-        (async () => {
-            updateProfile(auth.currentUser, name);
+  return (
+    <div className="max-w-6xl flex flex-col justify-center items-center mx-auto p-2 min-h-[calc(100vh-48px)]">
+      <h1 className="text-center text-3xl font-bold p-6 mb-6">Profile</h1>
+      <div className="w-full max-w-[500px]">
+        <input
+          type={"text"}
+          className={`w-full p-2 mb-4 rounded text-slate-700 ${
+            editChange && "bg-red-200 font-bold"
+          }`}
+          onChange={(e) =>
+            setInputValue((prevState) => {
+              return {
+                ...prevState,
+                [e.target.name]: e.target.value,
+              };
+            })
+          }
+          name={"name"}
+          value={name}
+          disabled={!editChange}
+        />
+        <input
+          type={"email"}
+          className={"w-full p-2 mb-4 rounded text-slate-700"}
+          value={email}
+          disabled={true}
+          name={"email"}
+        />
 
-            // update progile data on firestore
-
-            const user = auth.currentUser;
-
-            const docRef = doc(db, "users", user.uid);
-
-            await updateProfile(user, {
-                displayName: name,
-            });
-
-            await updateDoc(
-                docRef,
-                {
-                    name: name,
-                },
-                {merge: true}
-            )
-                .then(() => {
-                    toast.success("Update success");
-                })
-                .catch((error) => {
-                    console.log(error);
-                    toast.error("Update failed");
-                });
-        })();
-    }
-
-    useEffect(() => {
-        async function fetchUserListings() {
-            const listingRef = collection(db, "listings");
-            const q = query(
-                listingRef,
-                where("userRef", "==", auth.currentUser.uid),
-                orderBy("timestamp", "desc")
-            );
-            const querySnap = await getDocs(q);
-            let listings = [];
-            querySnap.forEach((doc) => {
-                return listings.push({
-                    id: doc.id,
-                    data: doc.data(),
-                });
-            });
-            console.log(querySnap);
-            setListings(listings);
-            setLoading(false);
-        }
-        fetchUserListings();
-    }, [auth.currentUser.uid]);
-
-    return (
-        <div className="max-w-6xl flex flex-col justify-center items-center mx-auto p-2 h-[calc(100vh-48px)]">
-            <h1 className="text-center text-3xl font-bold p-6 mb-6">Profile</h1>
-            <div className="w-full max-w-[500px]">
-                <input
-                    type={"text"}
-                    className={`w-full p-2 mb-4 rounded text-slate-700 ${
-                        editChange && "bg-red-200 font-bold"
-                    }`}
-                    onChange={(e) =>
-                        setInputValue((prevState) => {
-                            return {
-                                ...prevState,
-                                [e.target.name]: e.target.value,
-                            };
-                        })
-                    }
-                    name={"name"}
-                    value={name}
-                    disabled={!editChange}
-                />
-                <input
-                    type={"email"}
-                    className={"w-full p-2 mb-4 rounded text-slate-700"}
-                    value={email}
-                    disabled={true}
-                    name={"email"}
-                />
-
-                <div className="whitespace-nowrap flex justify-between w-full mb-4 text-sm md:text-base">
-                    <p>
-                        Do want to change your name?
-                        <span
-                            className="text-red-400 ml-1 cursor-pointer"
-                            onClick={() => {
-                                editChange && updateProfileName();
-                                editChangeFunc();
-                            }}
-                        >
+        <div className="whitespace-nowrap flex justify-between w-full mb-4 text-sm md:text-base">
+          <p>
+            Do want to change your name?
+            <span
+              className="text-red-400 ml-1 cursor-pointer"
+              onClick={() => {
+                editChange && updateProfileName();
+                editChangeFunc();
+              }}
+            >
               {editChange ? "Apply change" : "Edit"}
             </span>
-                    </p>
-                    <p
-                        onClick={onSignOut}
-                        className="text-blue-500 font-bold cursor-pointer"
-                    >
-                        Sign Out
-                    </p>
-                </div>
-
-                <button
-                    type="button"
-                    className=" w-full bg-sky-500 text-medium font-medium rounded px-7 py-3 shadow shadow-blue-400 hover:bg-blue-500 active:bg-blue-600 transition ease-in-out duration-300"
-                >
-                    <Link className="flex items-center justify-center" to={"/CreateItem"}>
-                        <FcHome className="mr-2 bg-white h-7 w-7 p-1 rounded-full"/>
-                        Sell or Rent you Home
-                    </Link>
-                </button>
-            </div>
-            <div className="max-w-6xl px-3 mx-auto mt-6">
-                {!loading && listings.length > 0 && (
-                    <>
-                        <h2 className="text-2xl text-center font-semibold">My Listings</h2>
-                        <ul>
-                            {
-                                listings.map((listing) => (
-                                    <ListingItem
-                                        key={listing.id}
-                                        id={listing.id}
-                                        listing={listing.data}
-                                    />
-                                ))
-                            }
-                        </ul>
-
-                    </>
-                )}
-            </div>
+          </p>
+          <p
+            onClick={onSignOut}
+            className="text-blue-500 font-bold cursor-pointer"
+          >
+            Sign Out
+          </p>
         </div>
-    );
+
+        <button
+          type="button"
+          className=" w-full bg-sky-500 text-medium font-medium rounded px-7 py-3 shadow shadow-blue-400 hover:bg-blue-500 active:bg-blue-600 transition ease-in-out duration-300"
+        >
+          <Link className="flex items-center justify-center" to={"/CreateItem"}>
+            <FcHome className="mr-2 bg-white h-7 w-7 p-1 rounded-full" />
+            Sell or Rent you Home
+          </Link>
+        </button>
+      </div>
+      <div className="max-w-6xl px-3 mx-auto mt-6">
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold">My Listings</h2>
+            <ul className="md:flex md:gap-8 md:flex-wrap md:justify-center mt-4">
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
